d098bcabaceffd09a7a87bd0a5f5c6069f4591cb
Dana L. Brown <dana.l.brown@gmail.com>
2019-07-29 15:27:19 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function precarica(img) {
     $(img).each(function () {
-      $('<img/>) [0].src = this;
+      $('<img/>') [0].src = this;
     });
   }
   //precarica means pre-load              
