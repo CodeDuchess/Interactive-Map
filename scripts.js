3c3f501f04e3dc866ce2f4031bb0aee11d685df8
Dana L. Brown <dana.l.brown@gmail.com>
2019-07-30 13:00:46 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,31 +17,31 @@ function precarica(img) {
             $('#map').attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
         }),
 			$('.title-blue').mouseover( function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-blue.png');
 			});
 			$('.title-red').mouseout(function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
 			}),
 			$('.title-red').mouseover( function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-red.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-red.png');
 			});
 			$('.title-green').mouseout(function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
 			}),
 			$('.title-green').mouseover( function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-green.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-green.png');
 			});	
 			$('.title-yellow').mouseout(function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
 			}),
 			$('.title-yellow').mouseover( function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-yellow.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-yellow.png');
 			});
 			$('.title-pink').mouseout(function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-original.png');
 			}),
 			$('.title-pink').mouseover( function(){
-				$("#map").attr('src', 'http://www.sarabianchi.it/code-images/mappa-interattiva/img/map-pink.png');
+				$("#map").attr('src', 'https://www.sarabianchi.it/code-images/mappa-interattiva/img/map-pink.png');
 			});	
 		});
               
